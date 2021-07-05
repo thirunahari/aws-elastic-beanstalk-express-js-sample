21fb01ca4c7408797cd43915ebbb85a09c2ac1eb
thirunahari <thirunahari@gmail.com>
2021-07-05 17:10:26 +0530

    mannual review

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('<h1>CI/CD is successfully Deployed.</h1>'));
+app.get('/', (req, res) => res.send('<h1>CI/CD is successfully Deployed with mannual Review.</h1>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
